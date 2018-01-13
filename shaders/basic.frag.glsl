df2a3a419b592afefb41ed98e74f91a00b55532a
Peter <PopThosePringles@users.noreply.github.com>
2018-01-13 02:32:42 +0000

    Fixed spacing

--- shaders/basic.frag.glsl
+++ shaders/basic.frag.glsl
@@ -12,7 +12,7 @@ uniform float _Height;
 uniform float _Time;
 
 void main(){
-    vec2 p = gl_FragCoord.xy / (_Width * 1.5);
+	vec2 p = gl_FragCoord.xy / (_Width * 1.5);
 
 	gl_FragColor = vec4(sin(p.x * _Time / 60.0), sin(p.y * _Time / 60.0), sin(p.x + 0.3 * _Time / 60.0), 1.0);
 }
